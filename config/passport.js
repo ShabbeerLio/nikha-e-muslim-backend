e55d5ecfbfe08f17b72f691a2a371eee343c0fc4
Shabbeer <shabbeer@Shabbeers-MacBook-Air.local>
2026-01-12 20:04:40 +0530

    updated

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: "/api/api/auth/google/callback",
     },
     async (_, __, profile, done) => {
       const email = profile.emails[0].value;
